08cf08b545de59924e0165998d4184151e4453bb
Alicia Remont Ospina <alicia.remosp@gmail.com>
2023-03-11 14:25:34 +0000

    changes in ml_logic and aadded interface folder

--- ml_logic/data.py
+++ ml_logic/data.py
@@ -4,32 +4,25 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    clean raw data by removing irrelevant columns
+    and renaming columns
+    """
 
-def read_data(df: pd.DataFrame) -> pd.DataFrame:
-    '''
-    docstring
-    '''
+    df = df.drop(columns=['isFlaggedFraud'])
 
     df = df.rename(columns={'oldbalanceOrg':'oldBalanceOrig', 'newbalanceOrig':'newBalanceOrig', \
                         'oldbalanceDest':'oldBalanceDest', 'newbalanceDest':'newBalanceDest'})
     df['errorBalanceOrig']=df['newBalanceOrig'] + df['amount'] - df['oldBalanceOrig']
     df['errorBalanceDest']=df['newBalanceDest'] + df['amount'] - df['oldBalanceDest']
-    return df
-
-
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    remove useless columns
-    """
-
-    # remove useless/redundant columns --> ASK CLAUDIA AND TAJ
-    df = df.drop(columns=['isFlaggedFraud'])
 
     return df
 
 def separate_feature_target (df: pd.DataFrame):
 
-    # Separate the features and target variable
+    """ Separate the features and target variables """
+
     X = df.drop('isFraud', axis=1)
     y = df['isFraud']
 
@@ -37,6 +30,17 @@ def separate_feature_target (df: pd.DataFrame):
 
 def split_data (X:pd.DataFrame, y:pd.Series):
 
+    """Split the data between train and test sets"""
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     return X_train, X_test, y_train, y_test
+
+def create_new_features (df:pd.DataFrame):
+
+    """Create 'balance error in originator account' feature and 'balance error in destination account feature """
+
+    df['errorBalanceOrig']=df['newBalanceOrig'] + df['amount'] - df['oldBalanceOrig']
+    df['errorBalanceDest']=df['newBalanceDest'] + df['amount'] - df['oldBalanceDest']
+
+    return df
--- ml_logic/encoders.py
+++ ml_logic/encoders.py
@@ -1,10 +1,29 @@
 import os
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import ADASYN
 from sklearn.preprocessing import OneHotEncoder
 from ml_logic.data import split_data, separate_feature_target
+
+
+
+# # Impute then scale numerical values:
+# num_transformer = Pipeline([
+#     ('imputer', SimpleImputer(strategy="mean")),
+#     ('standard_scaler', StandardScaler())
+# ])
+
+# # Encode categorical values
+# cat_transformer = OneHotEncoder(handle_unknown='ignore')
+
+# # Parallelize "num_transformer" and "cat_transfomer"
+# preprocessor = ColumnTransformer([
+#     ('num_transformer', num_transformer, ['age', 'bmi']),
+#     ('cat_transformer', cat_transformer, ['smoker', 'region'])
+# ])
 
 
 def transaction_type_encoder(df: pd.DataFrame) -> pd.DataFrame:
